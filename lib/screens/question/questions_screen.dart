409e1ae46af6d6ac6eaa2b102901a3495fad9c6c
Mustafa <mustafasmnc@gmail.com>
2022-11-14 19:38:56 +0300

    shimmering effect

--- lib/screens/question/questions_screen.dart
+++ lib/screens/question/questions_screen.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:study_app/controllers/question_paper/questions_controller.dart';
 import 'package:study_app/firebase_ref/loading_status.dart';
 import 'package:study_app/widgets/common/background_decoration.dart';
+import 'package:study_app/widgets/common/question_place_holder.dart';
+import 'package:study_app/widgets/content_area.dart';
 
 class QuestionsScreen extends GetView<QuestionsController> {
   const QuestionsScreen({Key? key}) : super(key: key);
@@ -15,15 +17,20 @@ class QuestionsScreen extends GetView<QuestionsController> {
           child: Obx(() => Column(
                 children: [
                   if (controller.loadingStatus.value == LoadingStatus.loading)
-                    const Expanded(child: Center(child: CircularProgressIndicator())),
+                    const Expanded(
+                        child: ContentArea(
+                      child: QuestionScreenHolder(),
+                    )),
                   if (controller.loadingStatus.value == LoadingStatus.completed)
                     Expanded(
-                        child: SingleChildScrollView(
-                            child: Column(
-                      children: [
-                        Text(controller.currentQuestion.value!.question),
-                      ],
-                    ))),
+                        child: ContentArea(
+                      child: SingleChildScrollView(
+                          child: Column(
+                        children: [
+                          Text(controller.currentQuestion.value!.question),
+                        ],
+                      )),
+                    )),
                 ],
               ))),
     );
